f1d260b44bb74680d7ee854383a8a42b174e025c
Md Junayed <devjunayed@gmail.com>
2024-09-27 01:12:31 +0600

    vercel issue fix trail -1

--- src/components/Home/Category/Category.tsx
+++ src/components/Home/Category/Category.tsx
@@ -3,16 +3,22 @@ import { Image } from "antd";
 import { CategoryDataType } from "../../Dashboard/Category/ManageCategory";
 
 const Category = () => {
-  const { data: categories, isLoading } = useGetCategoryQuery("");
+  const { data: categories, isLoading, isFetching } = useGetCategoryQuery("");
 
-  if (isLoading) {
-    return <div>Loading...</div>;
+  if (isLoading || isFetching) {
+    return <div className="text-center  text-green my-10">Loading...</div>;
   }
   return (
     <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-8 ">
       {categories?.map((category: CategoryDataType) => (
         <div className=" flex flex-col items-center justify-center">
-          <Image preview={false} className="drop-shadow-2xl w-full h-full rounded-lg" width={270} height={250} src={category.image} />
+          <Image
+            preview={false}
+            className="drop-shadow-2xl w-full h-full rounded-lg"
+            width={270}
+            height={250}
+            src={category.image}
+          />
           <h4 className="text-xl font-bold text-center">{category.name}</h4>
         </div>
       ))}
